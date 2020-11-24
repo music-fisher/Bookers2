ccd955de62b5e6ab7ce8fb18e5ca52d98f7529f9
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-11-24 10:33:28 +0000

    [Update]住所検索機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,17 @@ def after_sign_in_path_for(resource)
   current_user.save
   user_path(resource)
 end  
-  protected
+protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
-  end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_,
+    :address_city,
+    :address_street,
+    # :address_building
+  ])
+end
 end
